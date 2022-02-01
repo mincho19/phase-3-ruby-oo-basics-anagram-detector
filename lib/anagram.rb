29a4795f7a03c4627a7d8b65b55992a59d0442bb
Min Cho <minsangcho19@gmail.com>
2022-02-01 12:30:48 -0800

    commit tree 668ce6ff28261ad13e09b8a2e8fbd4369a3646a9

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -12,11 +12,12 @@ class Anagram
     def match(arrayofwords)
         wordmatch = []
         wordmatch = arrayofwords.filter{ |arrayword|
-            chararrayword = arrayword.chars
-            sortedarrayword = chararrayword.sort
-            charword = @word.chars
-            sortedword = charword.sort
-            sortedword == sortedarrayword
+            # chararrayword = arrayword.chars.sort
+            # sortedarrayword = chararrayword.sort
+            # charword = @word.chars.sort
+            # sortedword = charword.sort
+            # sortedword == sortedarrayword
+            @word.chars.sort == arrayword.chars.sort
             # binding.pry
         }
         wordmatch
