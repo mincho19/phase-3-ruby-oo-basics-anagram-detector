448af92f37e6e57142d9a96ef690a7e70e61ae1c
Min Cho <minsangcho19@gmail.com>
2022-02-01 12:23:26 -0800

    done

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,26 @@
 # Your code goes here!
+require 'pry'
+
+class Anagram
+    attr_reader :word
+    attr_reader :wordmatch
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(arrayofwords)
+        wordmatch = []
+        wordmatch = arrayofwords.filter{ |arrayword|
+            chararrayword = arrayword.chars
+            sortedarrayword = chararrayword.sort
+            charword = @word.chars
+            sortedword = charword.sort
+            sortedword == sortedarrayword
+            # binding.pry
+        }
+        wordmatch
+    end
+end
+
+# test = Anagram.new('listen')
